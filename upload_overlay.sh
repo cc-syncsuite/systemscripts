1240df48ebefa039437ed1832444692d502d1825
Alexander Surma <alexander.surma@gmail.com>
2010-03-17 02:47:32 +0000

    Corrected path determination

--- upload_overlay.sh
+++ upload_overlay.sh
@@ -2,15 +2,21 @@
 
 TMPDIR=/tmp/overlay
 
+absolutify() {
+	readlink -f $1
+}
+
 if [ -z "${IF}" ] ; then
 	IF=eth0
 fi
 
 MAC=$(cat /sys/class/net/eth0/address | sed 's/://g')
 mkdir -p "${TMPDIR}"
-for file in $@; do
-	mkdir -p "${TMPDIR}$(basename $file)"
-	cp -vr "$file" "${TMPDIR}"
+for file in "$@" ; do
+	NPATH=$(dirname $(absolutify $file))
+	mkdir -p "${TMPDIR}${NPATH}"
+	cp -vr "$file" "${TMPDIR}${NPATH}"
 done
 
 rsync -vvxra --delete --specials --devices --progress -I ${TMPDIR} rsync://root@192.168.1.23:/uploadrf/overlays/${MAC}
+rm -rf ${TMPDIR}
